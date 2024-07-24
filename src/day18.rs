90af9646dc32f6b526b120b7797732de1f8b238b
Zhang Erning <zhang@erning.com>
2024-07-25 00:23:47 +0800

    day 18. pick's theorem and shoelace formula

--- src/day18.rs
+++ src/day18.rs
@@ -43,7 +43,7 @@ pub fn part_one(input: &str) -> usize {
     let (mut x0, mut y0) = (0, 0);
     let (mut x1, mut y1) = (0, 0);
     let (mut x, mut y) = (0, 0);
-    let mut trench: Vec<(i32, i32)> = vec![(x, y)];
+    let mut trench: Vec<(i32, i32)> = vec![];
     for &(d, s) in plan.iter() {
         let (dx, dy) = DIRS[d];
         for _ in 0..s {
@@ -94,18 +94,56 @@ pub fn part_one(input: &str) -> usize {
         flood(0, y);
         flood(w - 1, y);
     }
-    // grid.iter().for_each(|row| {
-    //     row.iter().for_each(|ch| print!("{}", ch));
-    //     println!();
-    // });
     grid.iter()
         .map(|row| row.iter().filter(|&&ch| ch != '+').count())
         .sum()
 }
 
-pub fn part_two(input: &str) -> usize {
+pub fn part_two(input: &str) -> i64 {
     let plan = parse_input_hex(input);
-    0
+
+    let mut vertices: Vec<(i64, i64)> = vec![(0, 0)];
+    let mut boundary_count: i64 = 0;
+    let (mut x, mut y) = (0, 0);
+    for &(direction, count) in plan.iter() {
+        let (dx, dy) = DIRS[direction];
+        x += (dx * count) as i64;
+        y += (dy * count) as i64;
+        vertices.push((x, y));
+        boundary_count += count as i64;
+    }
+
+    // https://en.wikipedia.org/wiki/Pick%27s_theorem
+    // https://en.wikipedia.org/wiki/Shoelace_formula
+
+    // pick's theorem.
+    // Suppose that a polygon has integer coordinates for
+    // all of its vertices. Let i be the number of integer
+    // points interior to the polygon, and let b be the
+    // number of integer points on its boundary (including
+    // both vertices and points along the sides). Then the
+    // area A of this polygon is:
+    //
+    //   A = i + b/2 - 1
+    //
+    // trapezoid formula:
+    //
+    //   A = SUM[(y1 + y2) * (x1 - x2)] / 2
+    //   A = \frac{1}{2} \sum_{i=1}^{n}{(y_i+y_{i+1})(x_i-x_{i+1})}
+    //
+    // ==> i = A - b/2 + 1
+
+    let area = vertices
+        .windows(2)
+        .map(|v| {
+            let ((x1, y1), (x2, y2)) = (v[0], v[1]);
+            (y1 + y2) * (x1 - x2) / 2
+        })
+        .sum::<i64>()
+        .abs();
+
+    let interior_count = area - boundary_count / 2 + 1;
+    interior_count + boundary_count
 }
 
 #[cfg(test)]
@@ -117,6 +155,6 @@ mod tests {
     fn example() {
         let input = read_example(18);
         assert_eq!(part_one(&input), 62);
-        assert_eq!(part_two(&input), 0);
+        assert_eq!(part_two(&input), 952408144115);
     }
 }
