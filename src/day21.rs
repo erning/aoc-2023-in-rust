b850b3528f90a244c4a4ffa2e659abfb37cb3539
Zhang Erning <zhang@erning.com>
2024-10-12 19:50:06 +0800

    to be optimized

--- src/day21.rs
+++ src/day21.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 
@@ -34,12 +35,13 @@ fn parse_input(input: &str) -> Garden {
             );
             w = w.max(x)
         });
+    assert!(w == h);
     w += 1;
     h += 1;
     Garden { start, rocks, w, h }
 }
 
-fn count_plots(garden: &Garden, steps: usize) -> usize {
+fn count_plots(garden: &Garden, steps: usize) -> Vec<(i32, i32)> {
     let mut plots: Vec<(i32, i32)> = Vec::new();
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
     let mut queue: VecDeque<(usize, (i32, i32))> = VecDeque::new();
@@ -63,18 +65,131 @@ fn count_plots(garden: &Garden, steps: usize) -> usize {
             queue.push_back((step - 1, (x, y)));
         }
     }
-    plots.len()
+
+    plots
+}
+
+fn _part_one(garden: &Garden, steps: usize) -> usize {
+    count_plots(garden, steps).len()
+}
+
+fn _part_two(garden: &Garden, steps: usize) -> usize {
+    let w = garden.w as usize;
+    let d = steps / w;
+    let m = steps % w;
+    let v = 5 + d as i32 % 2;
+    // let v = d as i32 + 1;
+    let s = steps.min(w * (v as usize - 1) + m);
+    // let s = steps;
+    let plots = count_plots(garden, s);
+    if steps == s {
+        return plots.len();
+    }
+
+    println!("==== {} -> {} {} {}", steps, s, d, m);
+
+    let mut boxes: HashMap<(i32, i32), usize> = HashMap::new();
+    for j in -v..=v {
+        for i in -v..=v {
+            let c = plots
+                .iter()
+                .filter(|(x, y)| {
+                    *x >= i * garden.w
+                        && *x < (i + 1) * garden.w
+                        && *y >= j * garden.h
+                        && *y < (j + 1) * garden.h
+                })
+                .count();
+            boxes.insert((i, j), c);
+        }
+    }
+    for j in -v..=v {
+        for i in -v..=v {
+            print!("{:5}, ", boxes.get(&(i, j)).unwrap());
+        }
+        println!();
+    }
+    let c1 = boxes.get(&(0, 0)).unwrap();
+    let c2 = boxes.get(&(0, 1)).unwrap();
+    println!("c={}, {}", c1, c2);
+
+    let n1 = boxes.get(&(0, -v)).unwrap();
+    let n2 = boxes.get(&(0, -v + 1)).unwrap();
+    let n3 = boxes.get(&(0, -v + 2)).unwrap();
+    println!("n={}, {}, {}", n1, n2, n3);
+
+    let s1 = boxes.get(&(0, v)).unwrap();
+    let s2 = boxes.get(&(0, v - 1)).unwrap();
+    let s3 = boxes.get(&(0, v - 2)).unwrap();
+    println!("s={}, {}, {}", s1, s2, s3);
+
+    let e1 = boxes.get(&(v, 0)).unwrap();
+    let e2 = boxes.get(&(v - 1, 0)).unwrap();
+    let e3 = boxes.get(&(v - 2, 0)).unwrap();
+    println!("e={}, {}, {}", e1, e2, e3);
+
+    let w1 = boxes.get(&(-v, 0)).unwrap();
+    let w2 = boxes.get(&(-v + 1, 0)).unwrap();
+    let w3 = boxes.get(&(-v + 2, 0)).unwrap();
+    println!("w={}, {}, {}", w1, w2, w3);
+
+    let ne1 = boxes.get(&(1, -v + 1)).unwrap();
+    let ne2 = boxes.get(&(1, -v + 2)).unwrap();
+    let ne3 = boxes.get(&(1, -v + 3)).unwrap();
+    println!("ne={}, {}, {}", ne1, ne2, ne3);
+
+    let se1 = boxes.get(&(1, v - 1)).unwrap();
+    let se2 = boxes.get(&(1, v - 2)).unwrap();
+    let se3 = boxes.get(&(1, v - 3)).unwrap();
+    println!("se={}, {}, {}", se1, se2, se3);
+
+    let nw1 = boxes.get(&(-1, -v + 1)).unwrap();
+    let nw2 = boxes.get(&(-1, -v + 2)).unwrap();
+    let nw3 = boxes.get(&(-1, -v + 3)).unwrap();
+    println!("nw={}, {}, {}", nw1, nw2, nw3);
+
+    let sw1 = boxes.get(&(-1, v - 1)).unwrap();
+    let sw2 = boxes.get(&(-1, v - 2)).unwrap();
+    let sw3 = boxes.get(&(-1, v - 3)).unwrap();
+    println!("sw={}, {}, {}", sw1, sw2, sw3);
+
+    let mut answer = 0;
+    answer += n1 + n2 + n3;
+    answer += e1 + e2 + e3;
+    answer += s1 + s2 + s3;
+    answer += w1 + w2 + w3;
+
+    answer += ne1 * d + ne2 * (d - 1) + ne3 * (d - 2);
+    answer += se1 * d + se2 * (d - 1) + se3 * (d - 2);
+    answer += sw1 * d + sw2 * (d - 1) + sw3 * (d - 2);
+    answer += nw1 * d + nw2 * (d - 1) + nw3 * (d - 2);
+
+    answer += {
+        let n = (d - 2) / 2;
+        let a = (1 + n) * n / 2 * 8 + 1;
+        // println!("{:?}", (n, a));
+        a * c1
+    };
+
+    answer += {
+        let n = (d - 1) / 2;
+        // let b = (4 + (2 * n - 1) * 4) * n / 2;
+        let b = 4 * n * n;
+        // println!("{:?}", (n, b));
+        b * c2
+    };
+
+    answer
 }
 
 pub fn part_one(input: &str) -> usize {
     let garden = parse_input(input);
-    count_plots(&garden, 64)
+    _part_one(&garden, 64)
 }
 
 pub fn part_two(input: &str) -> usize {
     let garden = parse_input(input);
-    count_plots(&garden, 0)
-    // count_plots(&garden, 26501365)
+    _part_two(&garden, 26501365)
 }
 
 #[cfg(test)]
@@ -85,13 +200,14 @@ mod tests {
     #[test]
     fn example() {
         let input = read_example(21);
-        let parsed = parse_input(&input);
-        assert_eq!(count_plots(&parsed, 6), 16);
-        assert_eq!(count_plots(&parsed, 10), 50);
-        assert_eq!(count_plots(&parsed, 50), 1594);
-        assert_eq!(count_plots(&parsed, 100), 6536);
-        assert_eq!(count_plots(&parsed, 500), 167004);
-        assert_eq!(count_plots(&parsed, 1000), 668697);
-        assert_eq!(count_plots(&parsed, 5000), 16733044);
+        let garden = parse_input(&input);
+        assert_eq!(_part_one(&garden, 6), 16);
+        assert_eq!(_part_two(&garden, 6), 16);
+        assert_eq!(_part_two(&garden, 10), 50);
+        assert_eq!(_part_two(&garden, 50), 1594);
+        assert_eq!(_part_two(&garden, 100), 6536);
+        assert_eq!(_part_two(&garden, 500), 167004);
+        assert_eq!(_part_two(&garden, 1000), 668697);
+        assert_eq!(_part_two(&garden, 5000), 16733044);
     }
 }
