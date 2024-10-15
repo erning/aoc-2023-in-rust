fd8643680e575bdefdeee47fe9f5b1e80b16302a
Zhang Erning <zhang@erning.com>
2024-10-15 18:45:54 +0800

    day 22 on going

--- src/day22.rs
+++ src/day22.rs
@@ -1,72 +1,79 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum Oriention {
-    X,
-    Y,
-    Z,
-}
-
-type Cube = (i32, i32, i32);
-
-type Vector = (i32, i32, i32);
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone)]
 struct Brick {
-    a: Cube,
-    b: Cube,
+    i: usize,
+    x1: i32,
+    y1: i32,
+    z1: i32,
+    x2: i32,
+    y2: i32,
+    z2: i32,
+    supporting: Vec<usize>,
+    supported: Vec<usize>,
 }
 
 impl Brick {
-    fn new(a: Cube, b: Cube) -> Self {
+    fn new(
+        i: usize,
+        x1: i32,
+        y1: i32,
+        z1: i32,
+        x2: i32,
+        y2: i32,
+        z2: i32,
+    ) -> Self {
+        assert!(x1 <= x2 && y1 <= y2 && z1 <= z2);
         assert!(
-            (a.0 == b.0 && (a.1 == b.1 || a.2 == b.2))
-                || (a.1 == b.1 && a.2 == b.2)
+            (x1 == x2 && (y1 == y2 || z1 == z2)) || (y1 == y2 && z1 == z2)
         );
-        assert!(a.0 <= b.0 && a.1 <= b.1 && a.2 <= b.2);
-        Brick { a, b }
-    }
-
-    fn oriention(&self) -> Oriention {
-        match self.vector() {
-            (0, 0, 0) => panic!(),
-            (0, 0, _) => Oriention::Z,
-            (0, _, 0) => Oriention::Y,
-            (_, 0, 0) => Oriention::X,
-            _ => panic!(),
+        Brick {
+            i,
+            x1,
+            y1,
+            z1,
+            x2,
+            y2,
+            z2,
+            supporting: Vec::new(),
+            supported: Vec::new(),
         }
     }
 
-    fn vector(&self) -> Vector {
-        (
-            self.b.0 - self.a.0,
-            self.b.1 - self.a.1,
-            self.b.2 - self.a.2,
-        )
-    }
-
-    fn is_encountered(&self, other: &Brick) -> bool {
-        false
-    }
-
-    fn is_parallel(&self, other: &Brick) -> bool {
-        match (self.oriention(), other.oriention()) {
-            (Oriention::X, Oriention::X) => {
-                self.a.1 != other.a.1 || self.a.2 != other.a.2
+    fn is_encountered(&self, other: &Self) -> bool {
+        fn is_encountered_1d(a1: i32, a2: i32, b1: i32, b2: i32) -> bool {
+            assert!(a1 <= a2 && b1 <= b2);
+            if a2 < b1 {
+                // a1-----a2.....b1-----b2
+                false
+            } else if b2 < a1 {
+                // b1-----b2.....a1-----a2
+                false
+            } else {
+                true
             }
-            (Oriention::Y, Oriention::Y) => {
-                self.a.0 != other.a.0 || self.a.2 != other.a.2
-            }
-            (Oriention::Z, Oriention::Z) => {
-                self.a.0 != other.a.0 || self.a.1 != other.a.1
-            }
-            _ => false,
         }
+
+        is_encountered_1d(self.x1, self.x2, other.x1, other.x2)
+            && is_encountered_1d(self.y1, self.y2, other.y1, other.y2)
+            && is_encountered_1d(self.z1, self.z2, other.z1, other.z2)
+    }
+
+    fn fall_to(&mut self, z: i32) {
+        let d = self.z2 - self.z1;
+        self.z1 = z;
+        self.z2 = z + d;
+    }
+
+    fn up(&mut self) {
+        self.z1 += 1;
+        self.z2 += 1;
     }
 }
 
 fn parse_input(input: &str) -> Vec<Brick> {
     input
         .lines()
-        .map(|s| {
+        .enumerate()
+        .map(|(i, s)| {
             let parts: Vec<Vec<i32>> = s
                 .splitn(2, '~')
                 .map(|s| {
@@ -74,17 +81,65 @@ fn parse_input(input: &str) -> Vec<Brick> {
                 })
                 .collect();
             Brick::new(
-                (parts[0][0], parts[0][1], parts[0][2]),
-                (parts[1][0], parts[1][1], parts[1][2]),
+                i + 1,
+                parts[0][0],
+                parts[0][1],
+                parts[0][2],
+                parts[1][0],
+                parts[1][1],
+                parts[1][2],
             )
         })
         .collect()
 }
 
-pub fn part_one(input: &str) -> u32 {
-    let bricks = parse_input(input);
-    println!("{:?}", bricks);
-    0
+pub fn part_one(input: &str) -> usize {
+    let mut bricks = parse_input(input);
+    bricks.sort_unstable_by(|a, b| b.z1.cmp(&a.z1));
+    let mut settled: Vec<Brick> = Vec::new();
+    while let Some(mut brick) = bricks.pop() {
+        let mut z: Option<i32> = None;
+        for other in settled.iter_mut() {
+            match z {
+                Some(z) => {
+                    if z != other.z2 {
+                        break;
+                    }
+                }
+                None => {
+                    brick.fall_to(other.z2);
+                }
+            }
+            if brick.is_encountered(other) {
+                z = Some(brick.z1);
+                brick.supported.push(other.i);
+                other.supporting.push(brick.i);
+            }
+        }
+        if z.is_none() {
+            brick.fall_to(1);
+        } else {
+            brick.up();
+        }
+        settled.push(brick);
+        settled.sort_unstable_by(|a, b| b.z2.cmp(&a.z2));
+    }
+    for b in settled.iter() {
+        println!("{:?}", b);
+    }
+    let mut x = settled.len();
+    for b in settled.iter() {
+        let c = settled
+            .iter()
+            .filter(|v| v.supported.len() == 1 && v.supported.contains(&b.i))
+            .count();
+        if c > 0 {
+            x -= 1;
+        }
+        println!("{:?}", (c, b.i));
+    }
+
+    x
 }
 
 pub fn part_two(input: &str) -> u32 {
@@ -95,6 +150,15 @@ pub fn part_two(input: &str) -> u32 {
 mod tests {
     use super::*;
     use crate::read_example;
+
+    #[test]
+    fn test_encountered() {
+        let b1 = Brick::new(1, 1, 0, 0, 1, 2, 0);
+        let b2 = Brick::new(2, 0, 0, 2, 2, 0, 2);
+        let b3 = Brick::new(3, 0, 0, 0, 2, 0, 0);
+        assert_eq!(false, b1.is_encountered(&b2));
+        assert_eq!(true, b1.is_encountered(&b3));
+    }
 
     #[test]
     fn example() {
