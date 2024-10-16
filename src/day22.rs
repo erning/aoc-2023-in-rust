ef53e6c6e44332a97775829580eb814972a43b82
Zhang Erning <zhang@erning.com>
2024-10-16 17:04:30 +0800

    day 22 refactor

--- src/day22.rs
+++ src/day22.rs
@@ -1,71 +1,44 @@
+use std::collections::HashSet;
+
+#[derive(Debug, Clone)]
+struct Point3D {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+impl Point3D {
+    fn new(x: i32, y: i32, z: i32) -> Self {
+        Point3D { x, y, z }
+    }
+}
+
 #[derive(Debug, Clone)]
 struct Brick {
     i: usize,
-    x1: i32,
-    y1: i32,
-    z1: i32,
-    x2: i32,
-    y2: i32,
-    z2: i32,
-    supporting: Vec<usize>,
-    supported: Vec<usize>,
+    a: Point3D,
+    b: Point3D,
 }
 
 impl Brick {
-    fn new(
-        i: usize,
-        x1: i32,
-        y1: i32,
-        z1: i32,
-        x2: i32,
-        y2: i32,
-        z2: i32,
-    ) -> Self {
-        assert!(x1 <= x2 && y1 <= y2 && z1 <= z2);
-        assert!(
-            (x1 == x2 && (y1 == y2 || z1 == z2)) || (y1 == y2 && z1 == z2)
-        );
-        Brick {
-            i,
-            x1,
-            y1,
-            z1,
-            x2,
-            y2,
-            z2,
-            supporting: Vec::new(),
-            supported: Vec::new(),
-        }
+    fn new(i: usize, a: Point3D, b: Point3D) -> Self {
+        // assert!(x1 <= x2 && y1 <= y2 && z1 <= z2);
+        // assert!(
+        //     (x1 == x2 && (y1 == y2 || z1 == z2)) || (y1 == y2 && z1 == z2)
+        // );
+        Brick { i, a, b }
     }
 
     fn is_encountered(&self, other: &Self) -> bool {
-        fn is_encountered_1d(a1: i32, a2: i32, b1: i32, b2: i32) -> bool {
-            assert!(a1 <= a2 && b1 <= b2);
-            if a2 < b1 {
-                // a1-----a2.....b1-----b2
-                false
-            } else if b2 < a1 {
-                // b1-----b2.....a1-----a2
-                false
-            } else {
-                true
-            }
-        }
-
-        is_encountered_1d(self.x1, self.x2, other.x1, other.x2)
-            && is_encountered_1d(self.y1, self.y2, other.y1, other.y2)
-            && is_encountered_1d(self.z1, self.z2, other.z1, other.z2)
+        (self.b.x >= other.a.x && self.a.x <= other.b.x)
+            && (self.b.y >= other.a.y && self.a.y <= other.b.y)
+            && (self.b.z >= other.a.z && self.a.z <= other.b.z)
     }
 
-    fn fall_to(&mut self, z: i32) {
-        let d = self.z2 - self.z1;
-        self.z1 = z;
-        self.z2 = z + d;
-    }
-
-    fn up(&mut self) {
-        self.z1 += 1;
-        self.z2 += 1;
+    fn set_z(&mut self, z: i32) {
+        let d = self.b.z - self.a.z;
+        self.a.z = z;
+        self.b.z = z + d;
     }
 }
 
@@ -80,66 +53,52 @@ fn parse_input(input: &str) -> Vec<Brick> {
                     s.splitn(3, ',').map(|v| v.parse().unwrap()).collect()
                 })
                 .collect();
-            Brick::new(
-                i + 1,
-                parts[0][0],
-                parts[0][1],
-                parts[0][2],
-                parts[1][0],
-                parts[1][1],
-                parts[1][2],
-            )
+            let a = Point3D::new(parts[0][0], parts[0][1], parts[0][2]);
+            let b = Point3D::new(parts[1][0], parts[1][1], parts[1][2]);
+
+            Brick::new(i, a, b)
         })
         .collect()
 }
 
-pub fn part_one(input: &str) -> usize {
-    let mut bricks = parse_input(input);
-    bricks.sort_unstable_by(|a, b| b.z1.cmp(&a.z1));
+fn fall(bricks: &mut Vec<Brick>) -> (Vec<Vec<usize>>, Vec<Vec<usize>>) {
+    let n = bricks.len();
+    // let mut bricks = bricks.clone();
+    bricks.sort_unstable_by_key(|k| -k.a.z);
+    let mut supported: Vec<Vec<usize>> = vec![Vec::new(); n];
+    let mut supporting: Vec<Vec<usize>> = vec![Vec::new(); n];
     let mut settled: Vec<Brick> = Vec::new();
     while let Some(mut brick) = bricks.pop() {
-        let mut z: Option<i32> = None;
-        for other in settled.iter_mut() {
-            match z {
-                Some(z) => {
-                    if z != other.z2 {
-                        break;
-                    }
-                }
-                None => {
-                    brick.fall_to(other.z2);
-                }
+        let mut z = 0;
+        for other in settled.iter() {
+            if z == 0 {
+                brick.set_z(other.b.z);
+            } else if z != other.b.z {
+                break;
             }
             if brick.is_encountered(other) {
-                z = Some(brick.z1);
-                brick.supported.push(other.i);
-                other.supporting.push(brick.i);
+                z = brick.a.z;
+                supported[brick.i].push(other.i);
+                supporting[other.i].push(brick.i);
             }
         }
-        if z.is_none() {
-            brick.fall_to(1);
-        } else {
-            brick.up();
-        }
+        brick.set_z(z + 1);
         settled.push(brick);
-        settled.sort_unstable_by(|a, b| b.z2.cmp(&a.z2));
+        settled.sort_unstable_by_key(|k| -k.b.z);
     }
-    for b in settled.iter() {
-        println!("{:?}", b);
-    }
-    let mut x = settled.len();
-    for b in settled.iter() {
-        let c = settled
-            .iter()
-            .filter(|v| v.supported.len() == 1 && v.supported.contains(&b.i))
-            .count();
-        if c > 0 {
-            x -= 1;
-        }
-        println!("{:?}", (c, b.i));
-    }
+    bricks.append(&mut settled);
+    (supported, supporting)
+}
 
-    x
+pub fn part_one(input: &str) -> usize {
+    let mut bricks = parse_input(input);
+    let (supported, _) = fall(&mut bricks);
+    let set: HashSet<usize> = supported
+        .iter()
+        .filter(|v| v.len() == 1)
+        .map(|v| v[0])
+        .collect();
+    bricks.len() - set.len()
 }
 
 pub fn part_two(input: &str) -> u32 {
@@ -153,11 +112,11 @@ mod tests {
 
     #[test]
     fn test_encountered() {
-        let b1 = Brick::new(1, 1, 0, 0, 1, 2, 0);
-        let b2 = Brick::new(2, 0, 0, 2, 2, 0, 2);
-        let b3 = Brick::new(3, 0, 0, 0, 2, 0, 0);
-        assert_eq!(false, b1.is_encountered(&b2));
-        assert_eq!(true, b1.is_encountered(&b3));
+        let b1 = Brick::new(1, Point3D::new(1, 0, 0), Point3D::new(1, 2, 0));
+        let b2 = Brick::new(2, Point3D::new(0, 0, 2), Point3D::new(2, 0, 2));
+        let b3 = Brick::new(3, Point3D::new(0, 0, 0), Point3D::new(2, 0, 0));
+        assert!(!b1.is_encountered(&b2));
+        assert!(b1.is_encountered(&b3));
     }
 
     #[test]
