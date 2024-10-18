9546cbd245978ae3c7de75399bcc4ff671242687
Zhang Erning <zhang@erning.com>
2024-10-18 17:09:32 +0800

    day 23. should be improved for part two

--- src/day23.rs
+++ src/day23.rs
@@ -23,26 +23,26 @@ fn parse_input(input: &str) -> (Trails, i16, i16) {
     (trails, height, start)
 }
 
-pub fn part_one(input: &str) -> u16 {
-    let (trails, height, start) = parse_input(input);
-
-    fn dfs(
-        trails: &Trails,
-        visited: &mut HashSet<Pos>,
-        max_steps: &mut u16,
-        height: i16,
-        x: i16,
-        y: i16,
-        steps: u16,
-    ) {
-        if y == height - 1 {
-            *max_steps = steps.max(*max_steps);
-            println!("{:?}", (steps, max_steps));
-            return;
-        }
-        const DIRS: [Pos; 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
-        let dirs = match trails.get(&(x, y)) {
-            // Some(_) => &DIRS,
+fn dfs(
+    trails: &Trails,
+    visited: &mut HashSet<Pos>,
+    max_steps: &mut u16,
+    height: i16,
+    x: i16,
+    y: i16,
+    steps: u16,
+    is_part_two: bool,
+) {
+    if y == height - 1 {
+        *max_steps = steps.max(*max_steps);
+        // println!("{:?}", (steps, max_steps));
+        return;
+    }
+    const DIRS: [Pos; 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+    let dirs = if is_part_two {
+        &DIRS
+    } else {
+        match trails.get(&(x, y)) {
             Some(b'.') => &DIRS,
             Some(b'>') => &DIRS[0..1],
             Some(b'v') => &DIRS[1..2],
@@ -51,24 +51,60 @@ pub fn part_one(input: &str) -> u16 {
             _ => {
                 panic!();
             }
-        };
-        for (dx, dy) in dirs {
-            let (x, y) = (x + dx, y + dy);
-            if trails.contains_key(&(x, y)) && visited.insert((x, y)) {
-                dfs(trails, visited, max_steps, height, x, y, steps + 1);
-                visited.remove(&(x, y));
-            }
+        }
+    };
+    for (dx, dy) in dirs {
+        let (x, y) = (x + dx, y + dy);
+        if trails.contains_key(&(x, y)) && visited.insert((x, y)) {
+            dfs(
+                trails,
+                visited,
+                max_steps,
+                height,
+                x,
+                y,
+                steps + 1,
+                is_part_two,
+            );
+            visited.remove(&(x, y));
         }
     }
+}
+
+pub fn part_one(input: &str) -> u16 {
+    let (trails, height, start) = parse_input(input);
 
     let mut visited: HashSet<Pos> = HashSet::new();
     let mut max_steps = 0;
-    dfs(&trails, &mut visited, &mut max_steps, height, start, 0, 0);
+    dfs(
+        &trails,
+        &mut visited,
+        &mut max_steps,
+        height,
+        start,
+        0,
+        0,
+        false,
+    );
     max_steps
 }
 
-pub fn part_two(_input: &str) -> u16 {
-    0
+pub fn part_two(input: &str) -> u16 {
+    let (trails, height, start) = parse_input(input);
+
+    let mut visited: HashSet<Pos> = HashSet::new();
+    let mut max_steps = 0;
+    dfs(
+        &trails,
+        &mut visited,
+        &mut max_steps,
+        height,
+        start,
+        0,
+        0,
+        true,
+    );
+    max_steps
 }
 
 #[cfg(test)]
@@ -80,6 +116,6 @@ mod tests {
     fn example() {
         let input = read_example(23);
         assert_eq!(part_one(&input), 94);
-        assert_eq!(part_one(&input), 154);
+        assert_eq!(part_two(&input), 154);
     }
 }
