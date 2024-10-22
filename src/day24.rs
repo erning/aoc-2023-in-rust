fe429c2e9fa2b10737e3ffe3a08641bc1ba9eab2
Zhang Erning <zhang@erning.com>
2024-10-22 16:36:44 +0800

    day 24 part one

--- src/day24.rs
+++ src/day24.rs
@@ -1,14 +1,12 @@
 #[derive(Debug, Clone, Copy)]
 struct Line {
-    px: i32,
-    py: i32,
-    pz: i32,
-    vx: i32,
-    vy: i32,
-    vz: i32,
+    px: f32,
+    py: f32,
+    pz: f32,
+    vx: f32,
+    vy: f32,
+    vz: f32,
 }
-
-impl Line {}
 
 fn parse_input(input: &str) -> Vec<Line> {
     input
@@ -16,8 +14,8 @@ fn parse_input(input: &str) -> Vec<Line> {
         .map(|s| {
             s.split([',', '@'])
                 .map(|s| s.trim())
-                .map(|s| s.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
+                .map(|s| s.parse::<f32>().unwrap())
+                .collect::<Vec<f32>>()
         })
         .inspect(|v| assert_eq!(6, v.len()))
         .map(|v| Line {
@@ -31,13 +29,58 @@ fn parse_input(input: &str) -> Vec<Line> {
         .collect()
 }
 
-pub fn part_one(input: &str) -> u32 {
-    let x = parse_input(input);
-    println!("{:?}", x);
-    0
+pub fn part_one(input: &str) -> usize {
+    fn general_equation(a: &Line) -> (f32, f32, f32) {
+        (a.vy, -a.vx, a.vx * a.py - a.vy * a.px)
+    }
+
+    fn get_intersection(a: &Line, b: &Line) -> Option<(f32, f32)> {
+        let (a1, b1, c1) = general_equation(a);
+        let (a2, b2, c2) = general_equation(b);
+        match a1 * b2 - a2 * b1 {
+            0.0 => None,
+            m => {
+                let x = (c2 * b1 - c1 * b2) / m;
+                let y = (c1 * a2 - c2 * a1) / m;
+                Some((x, y))
+            }
+        }
+    }
+
+    let mut answer = 0;
+    let lines = parse_input(input);
+    let (min, max) = if lines.len() <= 5 {
+        (7.0, 27.0)
+    } else {
+        (200000000000000.0, 400000000000000.0)
+    };
+    for (i, a) in lines.iter().enumerate().take(lines.len() - 1) {
+        for b in lines.iter().skip(i + 1) {
+            if let Some((x, y)) = get_intersection(a, b) {
+                if x < min || x > max || y < min || y > max {
+                    continue;
+                }
+                if (a.vx < 0.0 && x > a.px) || (a.vx > 0.0 && x < a.px) {
+                    continue;
+                }
+                if (a.vy < 0.0 && y > a.py) || (a.vy > 0.0 && y < a.py) {
+                    continue;
+                }
+                if (b.vx < 0.0 && x > b.px) || (b.vx > 0.0 && x < b.px) {
+                    continue;
+                }
+                if (b.vy < 0.0 && y > b.py) || (b.vy > 0.0 && y < b.py) {
+                    continue;
+                }
+                answer += 1;
+            }
+        }
+    }
+    answer
 }
 
 pub fn part_two(input: &str) -> u32 {
+    let lines = parse_input(input);
     0
 }
 
@@ -49,7 +92,7 @@ mod tests {
     #[test]
     fn example() {
         let input = read_example(24);
-        assert_eq!(part_one(&input), 0);
-        assert_eq!(part_two(&input), 0);
+        assert_eq!(part_one(&input), 2);
+        assert_eq!(part_two(&input), 47);
     }
 }
