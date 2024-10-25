a0f1437aa87a7744dfdaf8bdcc16b5ac52bb4082
Zhang Erning <zhang@erning.com>
2024-10-25 19:00:04 +0800

    prepare day 25

--- src/day25.rs
+++ src/day25.rs
@@ -1,4 +1,39 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::hash::Hash;
+
+fn parse_input(input: &str) -> HashMap<&str, HashSet<&str>> {
+    let v: Vec<(&str, Vec<&str>)> = input
+        .lines()
+        .map(|line| {
+            let v: Vec<&str> =
+                line.splitn(2, ':').map(|s| s.trim()).collect();
+            (v[0], v[1].split_whitespace().collect())
+        })
+        .collect();
+    let mut map: HashMap<&str, HashSet<&str>> = HashMap::new();
+    let mut add_item = |a, b| {
+        if let Some(item) = map.get_mut(a) {
+            item.insert(b);
+        } else {
+            let item: HashSet<&str> = HashSet::new();
+            map.insert(a, item);
+        }
+    };
+    for (k, v) in v {
+        for b in v {
+            add_item(k, b);
+            add_item(b, k);
+        }
+    }
+    map
+}
+
 pub fn part_one(input: &str) -> u32 {
+    let x = parse_input(input);
+    for (k, v) in x.iter() {
+        println!("{:?}", (k, v));
+    }
     0
 }
 
