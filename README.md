25bb543de4f6f86120c6caf522eb2836760352fb
Zhang Erning <zhang@erning.com>
2024-10-28 17:39:23 +0800

    day 25. edmonds_karp by chargpt

--- README.md
+++ README.md
@@ -6,96 +6,127 @@ $ cargo run --release -- --time
 --- Day 1: Trebuchet?! ---
 Part One: 57346
 Part Two: 57345
+Duration: (81µs, 189µs)
 
 --- Day 2: Cube Conundrum ---
 Part One: 2265
 Part Two: 64097
+Duration: (124µs, 55µs)
 
 --- Day 3: Gear Ratios ---
 Part One: 520135
 Part Two: 72514855
+Duration: (116µs, 157µs)
 
 --- Day 4: Scratchcards ---
 Part One: 23941
 Part Two: 5571760
+Duration: (161µs, 159µs)
 
 --- Day 5: If You Give A Seed A Fertilizer ---
 Part One: 240320250
 Part Two: 28580589
+Duration: (31µs, 50µs)
 
 --- Day 6: Wait For It ---
 Part One: 440000
 Part Two: 26187338
+Duration: (2µs, 3µs)
 
 --- Day 7: Camel Cards ---
 Part One: 248559379
 Part Two: 249631254
+Duration: (234µs, 349µs)
 
 --- Day 8: Haunted Wasteland ---
 Part One: 17621
 Part Two: 20685524831999
+Duration: (556µs, 3.73ms)
 
 --- Day 9: Mirage Maintenance ---
 Part One: 1916822650
 Part Two: 966
+Duration: (157µs, 147µs)
 
 --- Day 10: Pipe Maze ---
 Part One: 6717
 Part Two: 381
+Duration: (2.518ms, 4.815ms)
 
 --- Day 11: Cosmic Expansion ---
 Part One: 9312968
 Part Two: 597714117556
+Duration: (152µs, 123µs)
 
 --- Day 12: Hot Springs ---
 Part One: 7191
 Part Two: 6512849198636
+Duration: (3.058ms, 84.17ms)
 
 --- Day 13: Point of Incidence ---
 Part One: 34889
 Part Two: 34224
+Duration: (233µs, 182µs)
 
 --- Day 14: Parabolic Reflector Dish ---
 Part One: 108144
 Part Two: 108404
+Duration: (68µs, 28.264ms)
 
 --- Day 15: Lens Library ---
 Part One: 494980
 Part Two: 247933
+Duration: (103µs, 312µs)
 
 --- Day 16: The Floor Will Be Lava ---
 Part One: 7242
 Part Two: 7572
+Duration: (2.235ms, 586.143ms)
 
 --- Day 17: Clumsy Crucible ---
 Part One: 724
 Part Two: 877
+Duration: (42.813ms, 140.92ms)
 
 --- Day 18: Lavaduct Lagoon ---
 Part One: 50603
 Part Two: 96556251590677
+Duration: (761µs, 56µs)
 
 --- Day 19: Aplenty ---
 Part One: 401674
 Part Two: 134906204068564
+Duration: (390µs, 309µs)
 
 --- Day 20: Pulse Propagation ---
 Part One: 832957356
 Part Two: 240162699605221
+Duration: (3.783ms, 16.152ms)
 
 --- Day 21: Step Counter ---
 Part One: 3532
 Part Two: 590104708070703
+Duration: (1.728ms, 145.871ms)
 
 --- Day 22: Sand Slabs ---
 Part One: 482
 Part Two: 103010
+Duration: (10.156ms, 270.241ms)
 
 --- Day 23: A Long Walk ---
 Part One: 1966
 Part Two: 6286
+Duration: (22.72ms, 3.155229s)
 
 --- Day 24: Never Tell Me The Odds ---
 Part One: 24627
 Part Two: 527310134398221
+Duration: (581µs, 71µs)
+
+--- Day 25: Snowverload ---
+Part One: 568214
+Part Two: fifty stars
+Duration: (1.876ms, 0ns)
 ```
+
+`Apple M1 Max`
--- src/day25.rs
+++ src/day25.rs
@@ -1,44 +1,133 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::hash::Hash;
+use std::collections::VecDeque;
 
-fn parse_input(input: &str) -> HashMap<&str, HashSet<&str>> {
-    let v: Vec<(&str, Vec<&str>)> = input
-        .lines()
-        .map(|line| {
-            let v: Vec<&str> =
-                line.splitn(2, ':').map(|s| s.trim()).collect();
-            (v[0], v[1].split_whitespace().collect())
-        })
-        .collect();
-    let mut map: HashMap<&str, HashSet<&str>> = HashMap::new();
+// Define type aliases for readability
+type Graph<'a> = HashMap<&'a str, HashMap<&'a str, i32>>;
+
+fn bfs<'a>(
+    residual: &Graph<'a>,
+    source: &'a str,
+    sink: &'a str,
+    parent: &mut HashMap<&'a str, &'a str>,
+) -> bool {
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::new();
+
+    queue.push_back(source);
+    visited.insert(source);
+
+    while let Some(u) = queue.pop_front() {
+        if let Some(neighbors) = residual.get(u) {
+            for (&v, &capacity) in neighbors {
+                if !visited.contains(v) && capacity > 0 {
+                    queue.push_back(v);
+                    visited.insert(v);
+                    parent.insert(v, u);
+
+                    if v == sink {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    false
+}
+
+fn edmonds_karp<'a>(
+    graph: &mut Graph<'a>,
+    source: &'a str,
+    sink: &'a str,
+) -> i32 {
+    let mut max_flow = 0;
+    let mut parent = HashMap::new();
+
+    while bfs(graph, source, sink, &mut parent) {
+        // Find the bottleneck capacity in the path from source to sink
+        let mut path_flow = i32::MAX;
+        let mut v = sink;
+
+        while v != source {
+            let u = parent[v];
+            let capacity = *graph.get(u).unwrap().get(v).unwrap();
+            path_flow = path_flow.min(capacity);
+            v = u;
+        }
+
+        // Update the capacities in the residual graph along the path
+        v = sink;
+        while v != source {
+            let u = parent[v];
+            *graph.get_mut(u).unwrap().get_mut(v).unwrap() -= path_flow;
+            *graph.entry(v).or_default().entry(u).or_insert(0) += path_flow;
+            v = u;
+        }
+
+        max_flow += path_flow;
+    }
+
+    max_flow
+}
+
+fn parse_input(input: &str) -> Graph {
+    let mut map = Graph::new();
     let mut add_item = |a, b| {
         if let Some(item) = map.get_mut(a) {
-            item.insert(b);
+            item.insert(b, 1);
         } else {
-            let item: HashSet<&str> = HashSet::new();
+            let mut item: HashMap<&str, i32> = HashMap::new();
+            item.insert(b, 1);
             map.insert(a, item);
         }
     };
-    for (k, v) in v {
-        for b in v {
-            add_item(k, b);
-            add_item(b, k);
-        }
-    }
+    input.lines().for_each(|line| {
+        let mut iter = line.splitn(2, ':');
+        let u = iter.next().unwrap().trim();
+        iter.next().unwrap().split_whitespace().for_each(|v| {
+            add_item(u, v);
+            add_item(v, u)
+        });
+    });
     map
 }
 
-pub fn part_one(input: &str) -> u32 {
-    let x = parse_input(input);
-    for (k, v) in x.iter() {
-        println!("{:?}", (k, v));
+fn find_source_part<'a>(
+    graph: &Graph<'a>,
+    source: &'a str,
+) -> HashSet<&'a str> {
+    let mut visited: HashSet<&str> = HashSet::new();
+    let mut queue = VecDeque::new();
+    queue.push_back(source);
+    while let Some(u) = queue.pop_front() {
+        if !visited.insert(u) {
+            continue;
+        }
+        graph[u].iter().filter(|(_, c)| *c > &0).for_each(|(v, _)| {
+            queue.push_back(v);
+        });
+    }
+    visited
+}
+
+pub fn part_one(input: &str) -> usize {
+    let graph = parse_input(input);
+    let source = graph.keys().next().unwrap();
+    for sink in graph.keys().skip(1) {
+        let mut graph = graph.clone();
+        let max_flow = edmonds_karp(&mut graph, source, sink);
+        if max_flow == 3 {
+            let source_part = find_source_part(&graph, source);
+            let a = source_part.len();
+            let b = graph.len() - a;
+            return a * b;
+        }
     }
     0
 }
 
-pub fn part_two(input: &str) -> u32 {
-    0
+pub fn part_two<'a>(_: &str) -> &'a str {
+    "fifty stars"
 }
 
 #[cfg(test)]
@@ -49,7 +138,6 @@ mod tests {
     #[test]
     fn example() {
         let input = read_example(25);
-        assert_eq!(part_one(&input), 0);
-        assert_eq!(part_two(&input), 0);
+        assert_eq!(part_one(&input), 54);
     }
 }
